b321802eec05285436d68d1b648d1b695abdd039
Aftermath001 <alvinasiachi@gmail.com>
2023-05-15 21:29:30 +0300

    Day1

---
+++ Day1/day1.py
@@ -0,0 +1,35 @@
+import math
+
+# ### Day 1: Python Basics
+# Start by installing Python and a text editor. Learn the basics of Python, such as: 
+#  variables, data types, Math Operators, Comments, Input() function, Print() function, The str(), int(), and float() Functions
+
+# #### Exercise
+# Simple Python Calculator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
+# ADDITION
+x = 15
+y = 19
+print(x+y)
+
+# SUBTRACTION
+x = 98
+y = 35
+print(x-y)
+
+# DELETION
+x = [15,23,88,90]
+del x[2]
+print (x)
+
+# DIVISION
+x = 55
+y = 5
+print(x // y)
+
+# POWER
+x = pow (5, 4)
+print(x)
+
+# SQUARE ROOT
+x = math.sqrt(144)
+print(x)
